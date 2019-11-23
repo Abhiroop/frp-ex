a38bcb2092e6b158392d851fdfc258e35a8564ce
Abhiroop Sarkar <asiamgenius@gmail.com>
2019-11-23 01:17:50 +0100

    Added support for clocks

--- app/Control/Synchronous.hs
+++ app/Control/Synchronous.hs
@@ -1,55 +1,78 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Synchronous where
 
 import Control.Applicative
 import qualified Data.Bits as B
 
-import Prelude hiding (or, not)
+import Prelude hiding (not, or)
 
 type Clock = Bool
 
-type Stream a = [a]
+defaultClkValue = False
+
+newtype Stream a =
+  Stream
+    { st :: [(a, Bool)]
+    }
+
+instance Functor Stream where
+  fmap f s = Stream $ fmap (\(val, clk) -> (f val, clk)) (st s)
 
 class Nil t where
   nil :: t
 
+-- writing instances for the base types to
+-- avoid unnecessary use of undecidable instances
 instance Nil Int where
   nil = minBound
+
 instance Nil Bool where
   nil = minBound
 
-fby :: Nil a => Stream a -> Stream a -> Stream a
-fby (x:_) ys = x : ys -- s ->> pre ys
+instance (Bounded a, Nil a) => Nil (a, Clock) where
+  nil = (minBound, minBound)
 
-pre :: Nil a => Stream a -> Stream a
-pre xs = nil : xs
+fby :: Nil a => Stream a -> Stream a -> Stream a
+fby s1 s2 = Stream $ go (st s1) (st s2)
+  where
+    go (x:_) ys = x : ys -- s ->> pre ys
+
+pre :: (Bounded a, Nil a) => Stream a -> Stream a
+pre xs = Stream $ nil : st xs
 
 (->>) :: Stream a -> Stream a -> Stream a
-(x : _) ->> (_ : ys) = x : ys
+s1 ->> s2 = Stream $ go (st s1) (st s2)
+  where
+    go (x:_) (_:ys) = x : ys
 
 arr :: (a -> b) -> Stream a -> Stream b
 arr = fmap
 
-zipW :: (a -> b -> c) -> Stream a -> Stream b -> Stream c
-zipW = zipWith
+-- assuming clk1 and clk2 are the same
+zipWSameClk :: (a -> b -> c) -> Stream a -> Stream b -> Stream c
+zipWSameClk f s1 s2 =
+  Stream $
+  zipWith (\(val1, clk1) (val2, clk2) -> (f val1 val2, clk1)) (st s1) (st s2)
 
 -- Example
 xor :: Stream Bool -> Stream Bool -> Stream Bool
-xor = zipWith B.xor
+xor = zipWSameClk B.xor
 
 (&) :: Stream Bool -> Stream Bool -> Stream Bool
-(&) = zipWith (B..&.)
+(&) = zipWSameClk (B..&.)
 
 or :: Stream Bool -> Stream Bool -> Stream Bool
-or = zipWith (B..|.)
+or = zipWSameClk (B..|.)
 
 not :: Stream Bool -> Stream Bool
-not = map B.complement
+not = fmap B.complement
 
 false :: Stream Bool
-false = repeat False
+false = Stream $ repeat (False, defaultClkValue)
 
 true :: Stream Bool
-true = repeat True
+true = Stream $ repeat (True, defaultClkValue)
 
 full_adder a b c = (s, co)
   where
@@ -57,4 +80,3 @@ full_adder a b c = (s, co)
     co = (a & b) `or` (b & c) `or` (a & c)
 
 edge c = false ->> c & not (pre c)
-
